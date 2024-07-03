980a032db6fb5a185eeee883767f4812b597bb3b
EddieSparrow <Ed26rus@yandex.ru>
2024-07-03 18:27:12 +0300

    feat: add page with api

--- class-components/src/App.tsx
+++ class-components/src/App.tsx
@@ -1,31 +1,52 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
 import './App.css';
+import { ChangeEvent, Component } from 'react';
 
-function App() {
-  const [count, setCount] = useState(0);
+class App extends Component {
+  state = {
+    input: localStorage.getItem('input') || '',
+  };
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
-    </>
-  );
+  handleSubmit = async () => {
+    const { input } = this.state;
+    const inputValue = input;
+    localStorage.setItem('input', inputValue);
+    try {
+      const result = await getValue(inputValue);
+      console.log(result);
+    } catch (e) {
+      console.log(e);
+    }
+    async function getValue(inputValue: string) {
+      const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${inputValue}`, {
+        method: 'GET',
+      });
+      return res.json();
+    }
+  };
+
+  handleInputChange = (event: ChangeEvent<HTMLInputElement>) => {
+    const inputValue = event.target.value.toString();
+    this.setState({ input: inputValue });
+    // localStorage.setItem('input', inputValue);
+  };
+
+  render() {
+    const { input } = this.state;
+
+    return (
+      <>
+        <div className="container">
+          <div className="search">
+            <input className="search-input" typeof="search-input" value={input} onChange={this.handleInputChange}></input>
+            <button className="search-button" onClick={this.handleSubmit}>
+              Search
+            </button>
+          </div>
+          <div className="results"></div>
+        </div>
+      </>
+    );
+  }
 }
 
 export default App;
